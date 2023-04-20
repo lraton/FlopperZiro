fdcf48284ff15b54358e2378683baab0373ec8db
lraton <notari.filippo@outlook.it>
2023-04-20 16:41:00 +0200

    Add emulation test

--- main/rfid.ino
+++ main/rfid.ino
@@ -56,11 +56,52 @@ void scanRfid() {
 }
 
 void emulateRfid() {
+  uint8_t target[] = {
+    0x8C,              // INIT AS TARGET
+    0x00,              // MODE -> BITFIELD
+    0x08, 0x00,        // SENS_RES - MIFARE PARAMS
+    0xdc, 0x44, 0x20,  // NFCID1T
+    0x60,              // SEL_RES
+    0x01, 0xfe,        // NFCID2T MUST START WITH 01fe - FELICA PARAMS - POL_RES
+    0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xc0,
+    0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7,  // PAD
+    0xff, 0xff,                                // SYSTEM CODE
+    0xaa, 0x99, 0x88, 0x77, 0x66, 0x55, 0x44,
+    0x33, 0x22, 0x11, 0x01, 0x00,  // NFCID3t MAX 47 BYTES ATR_RES
+    0x0d, 0x52, 0x46, 0x49, 0x44, 0x49, 0x4f,
+    0x74, 0x20, 0x50, 0x4e, 0x35, 0x33, 0x32  // HISTORICAL BYTES
+  };
+  /*  uint8_t target[] = {
+      PN532_COMMAND_TGINITASTARGET,
+      0x05,                  // MODE: 0x04 = PICC only, 0x01 = Passive only (0x02 = DEP only)
+
+      // MIFARE PARAMS
+      0x04, 0x00,         // SENS_RES (seeeds studio set it to 0x04, nxp to 0x08)
+      0x00, 0x00, 0x00,   // NFCID1t	(is set over sketch with setUID())
+      0x20,               // SEL_RES    (0x20=Mifare DelFire, 0x60=custom)
+
+      // FELICA PARAMS
+      0x01, 0xFE,         // NFCID2t (8 bytes) FeliCa NEEDS TO BEGIN WITH 0x01 0xFE!
+      0x05, 0x01, 0x86,
+      0x04, 0x02, 0x02,
+      0x03, 0x00,         // PAD (8 bytes)
+      0x4B, 0x02, 0x4F, 
+      0x49, 0x8A, 0x00,   
+      0xFF, 0xFF,         // System code (2 bytes)
+      
+      0x01, 0x01, 0x66,   // NFCID3t (10 bytes)
+      0x6D, 0x01, 0x01, 0x10,
+      0x02, 0x00, 0x00,
+
+	  0x00, // length of general bytes
+      0x00  // length of historical bytes
+  };
+  */
   scanbase();
   display.setCursor(33, 30);
   uint8_t apdubuffer[255] = {}, apdulen = 0;
-  nfc.AsTarget();
-  nfc.setDataTarget(uid, uidLength);        //Mimic a smart card response with a PPSE APDU
+  //n fc.AsTarget();
+  nfc.setDataTarget(target, sizeof(target));  //Mimic a smart card response with a PPSE APDU
   display.println("Sending...");
   battery();
   delay(2000);
